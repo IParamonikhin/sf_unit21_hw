2c0e6fc3852f45677061e941b528acfd0b748e2e
Ivan <paramonikhin.ivan@yandex.ru>
2023-05-20 14:46:04 +0300

    first result

---
+++ sf_unit21_hw/CustomView/CustomView.swift
@@ -0,0 +1,72 @@
+//
+//  CustomView.swift
+//  sf_unit21_hw
+//
+//  Created by Иван on 20.05.2023.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    var workingView: UIView!
+    var xibName: String = "CustomView"
+    
+    var isBig: Bool = false
+    
+    var setBackgroundColor: UIColor = UIColor.white{
+        didSet { workingView.backgroundColor = setBackgroundColor }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    func getFromXib() -> UIView{
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func doBigger() {
+        workingView.frame = CGRect(x: workingView.frame.minX,
+                                   y: workingView.frame.minY, width: workingView.frame.width + 30,
+                                   height: workingView.frame.height + 30)
+        cornerRadius(workingView)
+    }
+    
+    func cornerRadius(_ view: UIView) {
+        view.layer.cornerRadius = view.frame.height / 2
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        cornerRadius(self)
+        cornerRadius(workingView)
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(workingView)
+    }
+
+}
--- sf_unit21_hw/ViewController.swift
+++ sf_unit21_hw/ViewController.swift
@@ -9,11 +9,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var panOutlet: UIPanGestureRecognizer!
+    
+
+    var customViewArray = [CustomView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for index in 0...6{
+            let newCustomViewElement: CustomView = CustomView(frame: CGRect(x: 10, y: 100 + 100 * index, width: 90, height: 90))
+            customViewArray.append(newCustomViewElement)
+            let panAction = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+            customViewArray[index].addGestureRecognizer(panAction)
+            view.addSubview(customViewArray[index])
+        }
     }
 
+    @IBAction func panAction(_ gesture: UIPanGestureRecognizer) {
+        var isBig: Bool = false
+        struct Capture{
+            var x: Bool
+            var y: Bool
+        }
 
+        let gestureTranslation = gesture.translation(in: view)
+        
+        guard let gestureView = gesture.view else { return }
+        
+        for index in 0...customViewArray.count - 1{
+            if gestureView == customViewArray[index]{
+                if customViewArray[index].isBig{
+                    isBig = true
+                }
+            }
+        }
+        gestureView.center = CGPoint(x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        for index in 0...customViewArray.count - 1{
+            var capture: Capture = Capture(x: false, y: false)
+            let enemyView = customViewArray[index]
+            if !enemyView.isBig && !enemyView.isHidden && !isBig{
+                if gestureView != enemyView{
+                    
+                    for value in Int(enemyView.frame.minY)...Int(enemyView.frame.maxY) {
+                        if Int(gestureView.center.y) == value {
+                            capture.y = true
+                        }
+                    }
+                    for value in Int(enemyView.frame.minX)...Int(enemyView.frame.maxX) {
+                        if Int(gestureView.center.x) == value {
+                            capture.x = true
+                        }
+                    }
+                    
+                    if capture.x && capture.y{
+                        enemyView.doBigger()
+                        enemyView.setBackgroundColor = .systemIndigo
+                        gestureView.isHidden = true
+                        capture.x = false
+                        capture.y = false
+                        enemyView.isBig = true
+                    }
+                }
+            }
+        }
+        gesture.setTranslation(.zero, in: view)
+        
+        guard gesture.state == .ended else { return }
+        
+        
+    }
+    
 }
 
